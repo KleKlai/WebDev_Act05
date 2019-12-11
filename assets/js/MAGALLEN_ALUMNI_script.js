fc70172efaa5e2667a0fe893e30bc55a18538882
Kleklai <mmagallen_180000002192@uic.edu.ph>
2019-12-11 11:56:33 +0800

    Alumni Social Networking Site v1.0.2

---
+++ assets/js/MAGALLEN_ALUMNI_script.js
@@ -0,0 +1,19 @@
+// Menu Animation 
+$(function() {  
+    $('.btn-6')
+      .on('mouseenter', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+              $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+          $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
+
+//   Reference: https://codepen.io/kjbrum/pen/wBBLXx
