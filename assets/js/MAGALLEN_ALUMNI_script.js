465f77631e5e95417d35c7ea31f073ab218a5c32
Kleklai <mmagallen_180000002192@uic.edu.ph>
2019-12-14 11:44:30 +0800

    Add loading screen while page content is rendering

--- assets/js/MAGALLEN_ALUMNI_script.js
+++ assets/js/MAGALLEN_ALUMNI_script.js
@@ -1,19 +1,54 @@
-// Menu Animation 
-$(function() {  
-    $('.btn-6')
-      .on('mouseenter', function(e) {
-              var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-              $(this).find('span').css({top:relY, left:relX})
-      })
-      .on('mouseout', function(e) {
-              var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-          $(this).find('span').css({top:relY, left:relX})
-      });
-    $('[href=#]').click(function(){return false});
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+      }
+  }, 1000);
+  }
+
+  function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+  }
+
+  onReady(function() {
+  setVisible('.page', true);
+  setVisible('.box1', false);
+  setVisible('.box2', false);
+  setVisible('.box3', false);
+  setVisible('.box4', false);
   });
 
-//   Reference: https://codepen.io/kjbrum/pen/wBBLXx
+  function confirmDialog(){
+
+      var question = document.getElementById("post-question").value;
+
+      if(question === ""){
+          Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Looks like you dont have any question to ask!',
+              footer: '<a href>Why do I have this issue?</a>'
+          })
+      }else{
+          Swal.fire({
+          title: 'Are you sure?',
+          text: "You want to post this question publicly?",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#FFFFF',
+          confirmButtonText: 'Yes, post it!'
+          }).then((result) => {
+              if (result.value) {
+                  Swal.fire(
+                  'Hooraay!',
+                  'Your question has been post.',
+                  'success'
+                  )
+                  // Clear input field
+                  document.getElementById("post-question").value = "";
+              }
+          })
+      }
+  }
